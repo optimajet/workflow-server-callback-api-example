ed6d26ae594ed64893492bb2fbb33a3cfbac68fd
AndrewSolo <asolomonov@optimajet.com>
2022-06-16 19:40:01 +0300

    Documentation

--- WorkflowServer.CallbackApi/Controllers/ApiController.cs
+++ WorkflowServer.CallbackApi/Controllers/ApiController.cs
@@ -23,17 +23,28 @@ public class ApiController : ControllerBase
         _actionProvider = new ActionProvider();
         _conditionProvider = new ConditionProvider();
         _identityProvider = new IdentityProvider();
+        
+        //The parameters will not be saved, this is an example. You should add the provider as a dependency injection.
         _parameterProvider = new ParameterProvider();
     }
 
     #region Actions & Conditions Execution
 
+    /// <summary>
+    /// Returns a list of actions that can be executed on the callback server.
+    /// When the Workflow Server should execute an Action with a certain name,
+    /// it calls the action execution method on the server that has returned this
+    /// name in response to the request for the list of actions.
+    /// </summary>
     [HttpGet]
     public Task<IActionResult> GetActions(string schemeCode, string token)
     {
         return Task.FromResult<IActionResult>(Ok(ApiResponse.Ok(_actionProvider.ActionNames)));
     }
 
+    /// <summary>
+    /// Request to execute the action. Which of callback servers is called to execute this method, depends on the list of actions returned.
+    /// </summary>
     [HttpPost]
     public async Task<IActionResult> ExecuteAction(InstanceRequest request)
     {
@@ -42,12 +53,22 @@ public class ApiController : ControllerBase
         return Ok(ApiResponse.Ok());
     }
 
+    /// <summary>
+    /// Returns a list of conditions that can be executed on the callback server.
+    /// When the Workflow Server should execute a condition with a certain name,
+    /// it calls the condition execution method on the server that has returned
+    /// this name in response to the request for the list of conditions.
+    /// </summary>
     [HttpGet]
     public Task<IActionResult> GetConditions(string schemeCode, string token)
     {
         return Task.FromResult<IActionResult>(Ok(ApiResponse.Ok(_conditionProvider.ConditionNames)));
     }
 
+    /// <summary>
+    /// Request to execute the condition. Which of the callback servers
+    /// is called to execute this method, depends on the list of conditions returned.
+    /// </summary>
     [HttpPost]
     public async Task<IActionResult> ExecuteCondition(InstanceRequest request)
     {
@@ -63,12 +84,21 @@ public class ApiController : ControllerBase
 
     #region Authorization Rules Execution
 
+    /// <summary>
+    /// Returns a list of Authorization Rules that can be executed on the callback server.
+    /// When the Workflow Server should execute an Authorization Rule with a certain name,
+    /// it calls the Authorization Rule execution method on the server that has returned
+    /// this name in response to the request for the list of Authorization Rules.
+    /// </summary>
     [HttpGet]
     public Task<IActionResult> GetRules(string schemeCode, string token)
     {
         return Task.FromResult<IActionResult>(Ok(ApiResponse.Ok(_identityProvider.RuleNames)));
     }
 
+    /// <summary>
+    /// Check if the User has the access to the command.
+    /// </summary>
     [HttpPost]
     public async Task<IActionResult> CheckRule(CheckRuleRequest request)
     {
@@ -76,6 +106,9 @@ public class ApiController : ControllerBase
         return Ok(ApiResponse.Ok(result));
     }
 
+    /// <summary>
+    /// Returns the list of all Users who have the access.
+    /// </summary>
     [HttpPost]
     public async Task<IActionResult> GetIdentities(InstanceRequest request)
     {
@@ -89,6 +122,11 @@ public class ApiController : ControllerBase
 
     #region Remote Scheme Generation
 
+    /// <summary>
+    /// Workflow Engine supports scheme generation. If you include this method, then,
+    /// to initialize a new scheme, the Workflow Server calls it on all the connected
+    /// servers with the settings containing the address of this method.
+    /// </summary>
     [HttpPost]
     public Task<IActionResult> Generate(GenerateRequest request)
     {
@@ -103,6 +141,10 @@ public class ApiController : ControllerBase
 
     #region Event Handlers
 
+    /// <summary>
+    /// The ProcessStatusChanged event is called only after switching to
+    /// the statuses of Idled and Finalized. It is not called for subprocesses.
+    /// </summary>
     [HttpPost]
     public Task<IActionResult> ProcessStatusChanged(StatusChangedRequest request)
     {
@@ -114,6 +156,9 @@ public class ApiController : ControllerBase
         return Task.FromResult<IActionResult>(Ok(ApiResponse.Ok()));
     }
     
+    /// <summary>
+    /// The ProcessActivityChanged event.
+    /// </summary>
     [HttpPost]
     public Task<IActionResult> ProcessActivityChanged(ActivityChangedRequest request)
     {
@@ -125,6 +170,9 @@ public class ApiController : ControllerBase
         return Task.FromResult<IActionResult>(Ok(ApiResponse.Ok()));
     }
     
+    /// <summary>
+    /// The processLogs event.
+    /// </summary>
     [HttpPost]
     public Task<IActionResult> ProcessLog(LogRequest request)
     {
@@ -137,12 +185,18 @@ public class ApiController : ControllerBase
 
     #region Parameters Providing 
     
+    /// <summary>
+    /// Used to get parameters list.
+    /// </summary>
     [HttpGet]
     public Task<IActionResult> GetParameterNames(string schemeCode, string token)
     {
         return Task.FromResult<IActionResult>(Ok(ApiResponse.Ok(_parameterProvider.GetNames)));
     }
     
+    /// <summary>
+    /// The method returns a JSON serialized parameter value.
+    /// </summary>
     [HttpPost]
     public Task<IActionResult> GetParameter(ParameterRequest request)
     {
@@ -150,6 +204,9 @@ public class ApiController : ControllerBase
         return Task.FromResult<IActionResult>(Ok(ApiResponse.Ok(result)));
     }
     
+    /// <summary>
+    /// Used for set parameter value.
+    /// </summary>
     [HttpPost]
     public Task<IActionResult> SetParameter(ParameterRequest request)
     {
